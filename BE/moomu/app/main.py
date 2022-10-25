2b13addc08dc411a06ad95005b7a0b3664c3716c
parksanghyeop <mon2210@naver.com>
2022-10-25 17:21:48 +0900

    feat<BE>: faq api 라우터 작성

--- BE/moomu/app/main.py
+++ BE/moomu/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
-from app.api import users, regions, notices
+from app.api import users, regions, notices, faqs
 from app.db import database, models
 
 
@@ -27,6 +27,7 @@ app = get_application()
 app.include_router(users.router)
 app.include_router(regions.router)
 app.include_router(notices.router)
+app.include_router(faqs.router)
 
 
 # 루트 경로로 접속하면 Swagger 문서로 리다이렉트
