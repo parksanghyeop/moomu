bf13ad4e786db931063186fb6735a4f944230116
이길상 <oat641@gmail.com>
2022-10-31 11:24:30 +0900

    fix<BE>: 회원정보 조회

--- BE/moomu/app/api/users.py
+++ BE/moomu/app/api/users.py
@@ -22,8 +22,9 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), p
     return users
 
 
-@router.get("/{user_id}", response_model=User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+@router.get("/profile", response_model=User)
+def read_user(db: Session = Depends(get_db), payload: dict = Depends(validate_token)):
+    user_id = payload.get("id")
     db_user = user_crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
--- BE/moomu/app/service/jwt_service.py
+++ BE/moomu/app/service/jwt_service.py
@@ -20,7 +20,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/token")
 
 def generate_access_token(user: User):
     expire = datetime.utcnow() + ACCESS_EXPIRES
-    to_encode = {"exp": expire, "username": user.username, "region": user.region_id, "role": user.user_role}
+    to_encode = {"id": user.id, "exp": expire, "nickname": user.nickname, "region": user.region_id, "role": user.user_role}
     encode_jwt = jwt.encode(to_encode, settings.ACCESS_KEY, algorithm=ALGORITHM)
 
     return {"access_token": encode_jwt, "token_type": "bearer"}
@@ -35,7 +35,8 @@ def decode_token(token: str, db: Session):
     try:
         decode_token = jwt.decode(token, settings.ACCESS_KEY, ALGORITHM)
         # 아이디 확인
-        db_user = user_crud.get_user_by_username(db, username=decode_token.get("username"))
+        db_user = user_crud.get_user(db, user_id=decode_token.get("id"))
+
         if not db_user:
             raise JWTError
         return decode_token
