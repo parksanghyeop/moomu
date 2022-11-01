b0547470ae6aab09b10a283f99b637605a5317f8
parksanghyeop <mon2210@naver.com>
2022-11-01 17:36:44 +0900

    feat<BE>: 푸시알림 토큰 async store 및 db저장

--- FE/moomuApp/src/api/requests.tsx
+++ FE/moomuApp/src/api/requests.tsx
@@ -1,13 +1,16 @@
 const requests = {
   // 로그인
   login: "users/login",
-  logout: "logout", 
+  logout: "logout",
 
   // 회원가입
   register: "users/register",
   // 셔틀버스
   shuttlebus: "shuttlebus/bus",
-  shuttlebus_notion: "shuttlebus/bus/"
-}
+  shuttlebus_notion: "shuttlebus/bus/",
+
+  // 푸시알림 토큰
+  set_expoToken: "users/set_expoToken",
+};
 
 export default requests;
