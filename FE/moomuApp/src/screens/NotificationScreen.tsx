f52a06efdbf5cfdb0ab4b1b7d030cb55429fda45
parksanghyeop <mon2210@naver.com>
2022-11-04 14:10:58 +0900

    feat<FE>: 알림페이지UI 초안

--- FE/moomuApp/src/screens/NotificationScreen.tsx
+++ FE/moomuApp/src/screens/NotificationScreen.tsx
@@ -1,19 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, FlatList } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../types/StackNavigation';
 import { Logo3 } from '../components/logo';
+import Button1 from '../components/button1';
+import Button2 from '../components/button2';
+import { Notification } from '../components/notification';
+import instance from '../api/axios';
 
 type NotificationScreenProps = StackScreenProps<
   RootStackParamList,
   'Notification'
 >;
 
+const pressNotification = (notification: any) => {
+  console.log('pressNotification');
+  console.log(notification.alarm_type, notification.target_id);
+};
+
 const NotificationScreen: React.FC<NotificationScreenProps> = (props) => {
+  const [notifications, setNotifications] = useState<any>([]);
+
+  useEffect(() => {
+    instance.get('alarms?page=0').then((res) => {
+      console.log(res.data.items[0]);
+      setNotifications(res.data.items);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Logo3 content="notification" />
-      <Text>NotificationScreen</Text>
+      <ScrollView style={styles.notificationListContainer}>
+        {notifications.length > 0 ? (
+          notifications.map((notification: any) => (
+            <Notification
+              key={notification.id}
+              item={notification}
+              onPress={() => pressNotification(notification)}
+            />
+          ))
+        ) : (
+          <Text style={styles.emptyText}>알림이 없습니다.</Text>
+        )}
+      </ScrollView>
+      <View style={{ marginBottom: 60 }}>
+        <Button1 text="목록 지우기" />
+      </View>
     </View>
   );
 };
@@ -22,9 +55,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    // justifyContent: "center",
     paddingTop: 50,
     backgroundColor: '#fff',
+  },
+  notificationListContainer: {
+    marginTop: 20,
+    overflow: 'hidden',
+    width: '90%',
+  },
+  emptyText: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 40,
+    color: '#A0AEC0',
   },
 });
 
