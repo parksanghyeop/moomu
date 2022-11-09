821c84129412d69ff44ca583a8e692c4200cbdcd
parksanghyeop <mon2210@naver.com>
2022-11-09 16:43:34 +0900

    feat<FE>: 메인페이지 알람 갯수 API 적용

--- FE/moomuApp/src/components/logoContents/logoMainContent.tsx
+++ FE/moomuApp/src/components/logoContents/logoMainContent.tsx
@@ -2,28 +2,38 @@ import React from 'react';
 import { StyleSheet, Image, View, Text, ImageBackground } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import * as RootNavigation from '../../../RootNavigation';
+import instance from '../../api/axios';
 
 const LogoMainContent = (props: any) => {
-  return (
-    <Text
-      style={styles.logoContent}
-      onPress={() => {
-        console.log('clicked');
-        RootNavigation.navigate('Notification');
-      }}
-    >
-      <FontAwesome name="bell" size={14} /> 2
-    </Text>
-  );
+    const [notificationCount, setNotificationCount] = React.useState(0);
+
+    React.useEffect(() => {
+        instance.get('/alarms/count-not-read').then((response) => {
+            setNotificationCount(response.data);
+        });
+    }, []);
+
+    return (
+        <Text
+            style={styles.logoContent}
+            onPress={() => {
+                console.log('clicked');
+                RootNavigation.navigate('Notification');
+            }}
+        >
+            <FontAwesome name="bell" size={14} />{' '}
+            <Text>{notificationCount}</Text>
+        </Text>
+    );
 };
 
 const styles = StyleSheet.create({
-  logoContent: {
-    color: '#63B3ED',
-    textAlign: 'center',
-    marginTop: 20,
-    fontSize: 16,
-  },
+    logoContent: {
+        color: '#63B3ED',
+        textAlign: 'center',
+        marginTop: 20,
+        fontSize: 16,
+    },
 });
 
 export default LogoMainContent;
