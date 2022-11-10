3d4ddb967ca761447845dbc90cf7ef37b1fcb008
김찬일 <kci8311@gmail.com>
2022-11-10 17:57:44 +0900

    fix<FE>: Merge Conflict

--- FE/moomuweb/src/setupProxy.js
+++ FE/moomuweb/src/setupProxy.js
@@ -1,12 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(
-    "/map-direction-15/v1",
-    createProxyMiddleware({
-      target: "https://naveropenapi.apigw.ntruss.com",
-      secure: false,
-      changeOrigin: true,
-    })
-  );
+  // app.use(
+  //   createProxyMiddleware("/map-direction-15/v1", {
+  //     target: "https://naveropenapi.apigw.ntruss.com",
+  //     // secure: false,
+  //     changeOrigin: true,
+  //   })
+  // );
 };
